4012bbca11a03cace9216c2b0976907279c818c2
faberine <yashaswi.aryan99@gmail.com>
2023-08-15 20:08:58 +0530

    Added services

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const qrServices = require('../services/service.qrgeneration');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/test', qrServices.test);
+router.post('/genQR', qrServices.generateQR);
 
 module.exports = router;
